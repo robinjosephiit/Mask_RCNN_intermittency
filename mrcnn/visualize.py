d77b17ec319cd29d3cd20d5df175eab3d4e9b3cb
Robin Joseph <robinjosephiit@gmail.com>
2021-09-24 10:50:48 +0530

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -83,7 +83,7 @@ def apply_mask(image, mask, color, alpha=0.5):
 def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
                       figsize=(16, 16), ax=None,
-                      show_mask=False, show_mask_polygon=True, show_bbox=True, 
+                      show_mask=True, show_mask_polygon=True, show_bbox=True, 
                       colors=None, captions=None, show_caption=True, save_fig_path=None,
                       filter_classes=None, min_score=None):
 
